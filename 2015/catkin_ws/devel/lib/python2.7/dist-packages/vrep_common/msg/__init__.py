6bfbb5a5df4ecf5dca47419f77318675cfd6a2a7
carlfsmith <csmithfloyd@gmail.com>
2015-04-29 17:14:43 -0500

    Updated control node to handle joy initializing trigger values to 0 instead of 1 (it's released state). Added IR publishers to vrep and a node with subscribers/publishers (although there seems to be an issue taking in Point32 data).

--- 2015/catkin_ws/devel/lib/python2.7/dist-packages/vrep_common/msg/__init__.py
+++
@@ -1,7 +0,0 @@
-from ._JointSetStateData import *
-from ._VisionSensorDepthBuff import *
-from ._ObjectGroupData import *
-from ._VrepInfo import *
-from ._VisionSensorData import *
-from ._ForceSensorData import *
-from ._ProximitySensorData import *
