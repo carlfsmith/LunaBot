d6cff037066e69c0edb5abf406ecb175d09a72dd
severett5 <severett@email.uark.edu>
2015-05-01 14:23:25 -0500

    made changes to arduino files
    Cleaned up files and put notes them

---
+++ 2015/Arduino/LinearActuator/LinearActuator.ino
@@ -0,0 +1,84 @@
+//------------------------------------------------------
+// This code was designed to control the direction of
+// the bin and the excavator on the robot.
+//------------------------------------------------------
+
+#include <Wire.h>
+#define ADDRESS 0x08
+
+/*int  INA = A3;
+int  INB = 10;
+int  INH = A2;
+int  IS = A1;
+*/
+
+byte trav_dist;
+volatile char Command= '0';
+char Previous_Command = '0';
+
+// Set up for pins and to begin the i2c connection
+void setup() {
+	pinMode(A3, OUTPUT);
+  	pinMode(10, OUTPUT);
+	pinMode(A2, OUTPUT);
+ 	digitalWrite(A2, LOW);
+	digitalWrite(A3, LOW);
+  	digitalWrite(10, LOW);
+  	Wire.begin(ADDRESS);
+  	Wire.onReceive(receiveData);
+  	Wire.onRequest(sendData);
+}
+
+// Main loop to delay
+void loop() {
+  	delayMicroseconds(10); 
+}
+
+// Function to receive data directly from Raspberry Pi
+void receiveData(int byteCount){
+	// While loop to run when there is communication from Pi
+  	while(Wire.available()){
+		// Read in first varibale sent from Pi into Command variable
+		Command = char(Wire.read());
+		// Read in second variable sent from Pi into PWM_val variable
+    	trav_dist = int(Wire.read());
+		// Make sure the trav_dist does not overflow or underflow
+    	if(trav_dist > 255){
+      	  trav_dist = 255;
+    	}
+		else if(trav_dist < 0){
+			trav_dist =0;
+		}
+		// Call update function
+    	update();
+  	}  
+}
+
+// Function to send data back to Raspberry Pi if needed
+void sendData(){
+  	Wire.write(Previous_Command);
+}
+
+// Function that updates bin/excavator when command changes
+void update(){
+	// Chunk of code that moves bin/excavator up
+	if(Command =='u'){
+		digitalWrite(A2, LOW);
+    	digitalWrite(A3, HIGH);
+    	digitalWrite(10, LOW);
+    	digitalWrite(A2, HIGH); //turn on in one direction
+  	}
+	// Chunk of code that moves bin/excavator down
+ 	else if(Command =='d'){
+    	digitalWrite(A2, LOW);
+    	digitalWrite(A3, LOW);
+    	digitalWrite(10, HIGH);
+    	digitalWrite(A2, HIGH);
+  	}
+	// Chunk of code that disables movement on bin/excavator
+  	else if(Command ==' '){
+    	digitalWrite(A2, LOW);
+  	}
+	// Sets current command to previous command
+  	Previous_Command = Command;
+}
---
+++ 2015/Arduino/MotorController/MotorController.ino
@@ -0,0 +1,84 @@
+//------------------------------------------------------
+// This code was designed to directly control the
+// rotation and speed of the excavator and wheels 
+// on the robot.
+//------------------------------------------------------
+
+#include <Wire.h>
+#define ADDRESS 0x04
+
+// Variables with pin locations to keep organized
+int FOR = 9;
+int REV = 10;
+int DIS = 8;
+byte PWM_val = 150;
+volatile char Command = '0';
+char Previous_Command = '0';
+
+// Set up for pins and to begin the i2c connection
+void setup() {
+  pinMode(FOR,OUTPUT);
+  pinMode(REV,OUTPUT);
+  pinMode(DIS,OUTPUT);
+  digitalWrite(DIS,HIGH);
+  Wire.begin(ADDRESS);
+  Wire.onReceive(receiveData);
+  Wire.onRequest(sendData);
+}
+
+// Main loop to delay
+void loop(){
+    delayMicroseconds(50);
+}
+
+// Function to receive data directly from Raspberry Pi
+void receiveData(int byteCount){
+	// While loop to run when there is communication from Pi
+	while(Wire.available()){
+		// Read in first varibale sent from Pi into Command variable
+		Command = char(Wire.read());
+		// Read in second variable sent from Pi into PWM_val variable
+	  	PWM_val = int(Wire.read());
+		// Make sure the PWM_val does not overflow or underflow
+	  	if(PWM_val > 255){
+			PWM_val = 255;
+		}
+		else if(PWM_val < 0){
+			PWM_val =0;
+		}
+		// Call update function
+		update();
+	}  
+}
+
+// Function to send data back to Raspberry Pi if needed
+void sendData(){
+  Wire.write(Previous_Command);
+}
+
+// Function that updates wheels/excavator when command changes
+void update(){
+	// Chunk of code to control forward motion
+	if(Command =='f'){
+    digitalWrite(DIS,LOW);
+    delayMicroseconds(20);
+    digitalWrite(REV,LOW);
+    analogWrite(FOR,PWM_val);
+  }
+  // Chuck of code to control reverse motion
+  else if(Command =='r'){
+    digitalWrite(DIS,LOW);
+    delayMicroseconds(20);
+    digitalWrite(FOR,LOW);
+    analogWrite(REV, PWM_val);
+  }
+  // Chunk of code to completely disable wheel
+  else if(Command ==' '){
+    digitalWrite(DIS,HIGH);
+    digitalWrite(REV,LOW);
+    digitalWrite(FOR,LOW);
+  }
+  // Sets current command to previous command
+  Previous_Command = Command;
+}
+
