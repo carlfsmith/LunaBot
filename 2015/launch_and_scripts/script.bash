5f79d23c85189c8b42977d1beeeac3f27fa307eb
carlfsmith <cxs054@email.uark.edu>
2015-05-14 18:23:18 -0500

    Relocated And Organized Code
    src folder contains code for ROS package as well as test files, shell
    scripts, and launch files. Added code for control script which does
    angled-based turning -rather than the more continuous motion in
    control.py code.

--- 2015/launch_and_scripts/script.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo chmod +x /dev/input/js0
-
-echo "Executing the launch file"
-#roslaunch razorbot testMotor.launch
-echo "Done"
---
+++ 2015/src/pi.bash
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "running all nodes..."
+
+echo "starting controller..."
+rosrun beginner_tutorals control.py
+echo "controller started..."
+
+echo "starting converter..."
+rosrun beginner_tutorals control_to_robot.py
+echo "converter started..."
+
+echo "starting bin..."
+rosrun beginner_tutorals bin.py
+echo "bin started..."
+
+echo "starting extractor..."
+rosrun beginner_tutorals extractor.py
+echo "extractor started..."
+
+echo "starting exctractor rotator..."
+rosrun beginner_tutorials extractor.py
+echo "exctractor rotator started..."
+
+echo "starting kill switch..."
+rosrun beginner_tutorials killSwitch.py
+echo "kill switch started..."
+
+echo "all nodes are running..."
